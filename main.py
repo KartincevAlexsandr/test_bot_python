02b619d29b738163f092d56871813bae4d0d654d
skat <skat@mail.ru>
2023-04-26 17:27:59 +0300

    поменяли

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
-import telebot, wikipedia, re
-# Создаем экземпляр бота
-bot = telebot.TeleBot('Здесь впиши токен, полученный от @botfather')
-# Устанавливаем русский язык в Wikipedia
-wikipedia.set_lang("ru")
-# Чистим текст статьи в Wikipedia и ограничиваем его тысячей символов
-def getwiki(s):
-    try:
-        ny = wikipedia.page(s)
-        # Получаем первую тысячу символов
-        wikitext=ny.content[:1000]
-        # Разделяем по точкам
-        wikimas=wikitext.split('.')
-        # Отбрасываем всЕ после последней точки
-        wikimas = wikimas[:-1]
-        # Создаем пустую переменную для текста
-        wikitext2 = ''
-        # Проходимся по строкам, где нет знаков «равно» (то есть все, кроме заголовков)
-        for x in wikimas:
-            if not('==' in x):
-                    # Если в строке осталось больше трех символов, добавляем ее к нашей переменной и возвращаем утерянные при разделении строк точки на место
-                if(len((x.strip()))>3):
-                   wikitext2=wikitext2+x+'.'
-            else:
-                break
-        # Теперь при помощи регулярных выражений убираем разметку
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2=re.sub('\{[^\{\}]*\}', '', wikitext2)
-        # Возвращаем текстовую строку
-        return wikitext2
-    # Обрабатываем исключение, которое мог вернуть модуль wikipedia при запросе
-    except Exception as e:
-        return 'В энциклопедии нет информации об этом'
-# Функция, обрабатывающая команду /start
+import telebot
+import random
+from telebot import types
+# Загружаем список интересных фактов
+f = open('data/facts.txt', 'r', encoding='UTF-8')
+facts = f.read().split('\n')
+f.close()
+# Загружаем список поговорок
+f = open('data/thinks.txt', 'r', encoding='UTF-8')
+thinks  = f.read().split('\n')
+f.close()
+# Создаем бота
+bot = telebot.TeleBot('Здесь твой токен, полученный от @botfather')
+# Команда start
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Отправьте мне любое слово, и я найду его значение на Wikipedia')
+        # Добавляем две кнопки
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Факт")
+        item2=types.KeyboardButton("Поговорка")
+        markup.add(item1)
+        markup.add(item2)
+        bot.send_message(m.chat.id, 'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты ',  reply_markup=markup)
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    bot.send_message(message.chat.id, getwiki(message.text))
+    # Если юзер прислал 1, выдаем ему случайный факт
+    if message.text.strip() == 'Факт' :
+            answer = random.choice(facts)
+    # Если юзер прислал 2, выдаем умную мысль
+    elif message.text.strip() == 'Поговорка':
+            answer = random.choice(thinks)
+    # Отсылаем юзеру сообщение в его чат
+    bot.send_message(message.chat.id, answer)
 # Запускаем бота
 bot.polling(none_stop=True, interval=0)
